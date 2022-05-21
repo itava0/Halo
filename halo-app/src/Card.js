121da7d48ddf05dfa24cddfe6dac43407b841d22
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-05-21 13:24:39 -0400

    Created a Card component to stored the info of the robots

---
+++ halo-app/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+return (
+  <div>
+    <img src="" alt="robots image" />
+    <div>
+      <h2>Jane Doe</h2>
+      <p>Jane.doe@gmail.com</p>
+    </div>
+  </div>
+);
+}
+
+
+export default Card;
