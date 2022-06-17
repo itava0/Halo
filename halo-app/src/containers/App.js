0f046c9920a841db21c3115962b889cf0ff6bc3e
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-17 00:06:52 -0400

    playing around with useEffect

--- halo-app/src/containers/App.js
+++ halo-app/src/containers/App.js
@@ -8,12 +8,14 @@ import "./app.css";
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setRobots(users));
-  }, []);
+      console.log(count);
+  }, [count]); // only run if count changes
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
@@ -28,7 +30,7 @@ function App() {
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <button onClick={setCount}>Click Me!</button>
+        <button onClick={() => setCount(count + 1)}>Click Me!</button>
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErroBoundary>
